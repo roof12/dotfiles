527e6ac33caae5f2bd8c893cf9d9a59f5fd3ef29
Scott Lewis <scott@roof12.com>
2012-07-20 14:13:22 -0600

    turn off zsh IGNORE_EOF AND CORRECT

--- zsh/config.zsh
+++ zsh/config.zsh
@@ -24,9 +24,9 @@ setopt HIST_VERIFY
 setopt SHARE_HISTORY # share history between sessions ???
 setopt EXTENDED_HISTORY # add timestamps to history
 setopt PROMPT_SUBST
-setopt CORRECT
+# turn this thing off until i figure out how to make it behave
+# setopt CORRECT
 setopt COMPLETE_IN_WORD
-setopt IGNORE_EOF
 
 setopt APPEND_HISTORY # adds history
 setopt INC_APPEND_HISTORY SHARE_HISTORY  # adds history incrementally and share it across sessions
