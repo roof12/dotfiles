c12dfa5d1252a565ff15b627b285118d92720ff6
Scott Lewis <scott@roof12.com>
2013-02-06 10:34:22 -0700

    fix zsh git completion deprecation warning

--- git/completion.sh
+++ git/completion.sh
@@ -1,9 +1,16 @@
 # Uses git's autocompletion for inner commands. Assumes an install of git's
 # bash `git-completion` script at $completion below (this is where Homebrew
 # tosses it, at least).
-completion=/usr/local/etc/bash_completion.d/git-completion.bash
 
-if test -f $completion
+# from brew's zsh
+zsh_completion=/usr/local/share/zsh/site-functions/git-flow-completion.zsh
+bash_completion=/usr/local/etc/bash_completion.d/git-completion.bash
+
+if test -f $zsh_completion
 then
-  source $completion
+  source $zsh_completion
+elif test -f $bash_completion
+then
+  source $bash_completion
+  echo Run 'brew install git-flow to fix deprecation error.'
 fi
